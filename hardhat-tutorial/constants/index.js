92a5aa66e06c6853e1016e873d1c87faa9156aa1
KhairallahA <khairallahfreelancer@gmail.com>
2022-09-20 13:23:33 +0300

    Deploy Project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7e235125f57b956559e40dfb17a8806320cdca50";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://gateway.pinata.cloud/ipfs/QmZTfMSLeihKC7Dd65UqTP6AmArcj3NiZ4dvm95y9tf7kW";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
